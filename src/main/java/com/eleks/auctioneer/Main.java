865b9c9fa09fe73473696e0c0493ad1f72955fae
VladBrendak <brendakvlad@gmail.com>
2023-06-12 21:02:03 +0300

    changed directory name and added some fixes

--- src/main/java/com/eleks/auctioneer/Main.java
+++ src/main/java/com/eleks/auctioneer/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.eleks.auctioneer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/eleks/auctioneer/MainController.java
+++ src/main/java/com/eleks/auctioneer/MainController.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.eleks.auctioneer;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
