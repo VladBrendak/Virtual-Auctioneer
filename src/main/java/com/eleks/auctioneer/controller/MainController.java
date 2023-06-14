814e93114834a2de238d9bfcb411e25d502af39b
VladBrendak <brendakvlad@gmail.com>
2023-06-14 16:01:15 +0300

    add some dependencies

--- src/main/java/com/eleks/auctioneer/controller/MainController.java
+++ src/main/java/com/eleks/auctioneer/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.eleks.auctioneer;
+package com.eleks.auctioneer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
