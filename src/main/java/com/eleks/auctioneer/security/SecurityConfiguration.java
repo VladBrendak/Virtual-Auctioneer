6502664615dae8a7f4c4b2f2cc60b14c8f9839e5
VladBrendak <brendakvlad@gmail.com>
2023-06-16 14:13:33 +0300

    added authorization

--- src/main/java/com/eleks/auctioneer/security/SecurityConfiguration.java
+++ src/main/java/com/eleks/auctioneer/security/SecurityConfiguration.java
@@ -3,10 +3,14 @@ package com.eleks.auctioneer.security;
 import com.eleks.auctioneer.service.UserInfoUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -33,7 +37,7 @@ public class SecurityConfiguration {
 
 
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+    public UserDetailsService userDetailsService(){
         return new UserInfoUserDetailsService();
     }
 
@@ -43,10 +47,13 @@ public class SecurityConfiguration {
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/user/new").permitAll())
-
+                        .requestMatchers("/", "/user/new").permitAll()
+                )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/other").authenticated());
+                        .requestMatchers("/other").authenticated()
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll
+                );
 
         return http.build();
     }
@@ -54,5 +61,14 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider()
+    {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
     }
 }
