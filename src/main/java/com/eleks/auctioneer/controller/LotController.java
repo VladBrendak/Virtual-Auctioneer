aad98ac63bf0d5f5eb0661bc84dc7799c05ab385
n1kcat <nikitchukkateryna@gmail.com>
2023-06-26 11:28:21 +0300

    Added submit lot endpoint

---
+++ src/main/java/com/eleks/auctioneer/controller/LotController.java
@@ -0,0 +1,44 @@
+package com.eleks.auctioneer.controller;
+
+
+import com.eleks.auctioneer.DTO.LotDTO;
+import com.eleks.auctioneer.entity.Lot;
+import com.eleks.auctioneer.service.LotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/lot")
+public class LotController {
+
+    @Autowired
+    private LotService lotService;
+
+//    @PostMapping("/posts")
+//    public String addNewLot(@Validated @RequestBody LotDTO lotDTO) {
+//        return lotService.addLot(lotDTO);
+//    }
+//
+//    @GetMapping("/posts")
+//    public List<LotDTO> getAllLot()
+//    {
+//        return lotService.getAllLot();
+//    }
+
+    @PostMapping("/addlots")
+    public ResponseEntity<String> uploadLot(@RequestParam LotDTO lotDTO,
+                                            @RequestParam("file") MultipartFile file,
+                                            @RequestParam("previewImage") MultipartFile previewImage)  {
+        return lotService.uploadLot(lotDTO, file, previewImage);
+    }
+
+
+
+}
---
+++ src/main/java/com/eleks/auctioneer/service/LotService.java
@@ -0,0 +1,43 @@
+package com.eleks.auctioneer.service;
+
+import com.eleks.auctioneer.DTO.LotDTO;
+import com.eleks.auctioneer.entity.Lot;
+import com.eleks.auctioneer.repository.LotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+@Service
+public class LotService {
+
+@Autowired
+    private LotRepository lotRepository;
+
+    public ResponseEntity<String> uploadLot(LotDTO lotDTO, MultipartFile file, MultipartFile Image) {
+
+        if (lotDTO.getName_of_lot() == null || file.isEmpty() || Image.isEmpty()) {
+            return ResponseEntity.badRequest().body("Lot name, file, and preview image are mandatory.");
+        }
+
+        String assetFileName = saveFile(file, "${FILE_PATH}");
+        String previewImageName = saveFile(Image, "${IMAGE_PATH}");
+
+        Lot lot = lotDTO.mapToLot(lotDTO);
+        lot.setAsset_file(assetFileName);
+        lot.setImage(previewImageName);
+
+        lotRepository.save(lot);
+
+        return ResponseEntity.ok("Lot submitted successfully.");
+    }
+    private String saveFile(MultipartFile file, String storagePath) {
+
+        String fileName = UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
+
+        return fileName;
+    }
+
+}
