44c997d3e496b2cf88b5b04064ac44ecfaa4df8d
VladBrendak <brendakvlad@gmail.com>
2023-06-29 13:10:27 +0300

    add some file save features

--- src/main/java/com/eleks/auctioneer/service/LotService.java
+++ src/main/java/com/eleks/auctioneer/service/LotService.java
@@ -4,17 +4,25 @@ import com.eleks.auctioneer.DTO.LotDTO;
 import com.eleks.auctioneer.entity.Lot;
 import com.eleks.auctioneer.repository.LotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class LotService {
+    @Value("spring.storage.file-path")
+    private String filePath;
 
-@Autowired
+    @Value("spring.storage.image-path")
+    private String imagePath;
+
+    @Autowired
     private LotRepository lotRepository;
 
     public ResponseEntity<String> uploadLot(LotDTO lotDTO, MultipartFile file, MultipartFile Image) {
@@ -23,10 +31,10 @@ public class LotService {
             return ResponseEntity.badRequest().body("Lot name, file, and preview image are mandatory.");
         }
 
-        String assetFileName = saveFile(file, "${FILE_PATH}");
-        String previewImageName = saveFile(Image, "${IMAGE_PATH}");
+        String assetFileName = saveFile(file, filePath);
+        String previewImageName = saveFile(Image, imagePath);
 
-        Lot lot = lotDTO.mapToLot(lotDTO);
+        Lot lot = LotDTO.mapToLot(lotDTO);
         lot.setAsset_file(assetFileName);
         lot.setImage(previewImageName);
 
@@ -37,6 +45,16 @@ public class LotService {
     private String saveFile(MultipartFile file, String storagePath) {
 
         String fileName = UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
+        String filePath = storagePath + "/" + fileName;
+
+        File destination = new File(filePath);
+        try {
+            file.transferTo(destination);
+        } catch (IOException ex)
+        {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
 
         return fileName;
     }
