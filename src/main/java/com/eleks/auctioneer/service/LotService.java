52f10e19a9872c5d7e1352105934bb623fa347ec
n1kcat <nikitchukkateryna@gmail.com>
2023-06-29 11:54:29 +0300

    Some changes

--- src/main/java/com/eleks/auctioneer/service/LotService.java
+++ src/main/java/com/eleks/auctioneer/service/LotService.java
@@ -8,23 +8,31 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class LotService {
 
-@Autowired
+    @Autowired
     private LotRepository lotRepository;
 
-    public ResponseEntity<String> uploadLot(LotDTO lotDTO, MultipartFile file, MultipartFile Image) {
+    public ResponseEntity<String> uploadLot(LotDTO lotDTO, MultipartFile file, MultipartFile previewImage) throws IOException {
 
-        if (lotDTO.getName_of_lot() == null || file.isEmpty() || Image.isEmpty()) {
+        if (lotDTO.getName_of_lot() == null || file.isEmpty() || previewImage.isEmpty()) {
             return ResponseEntity.badRequest().body("Lot name, file, and preview image are mandatory.");
         }
 
         String assetFileName = saveFile(file, "${FILE_PATH}");
-        String previewImageName = saveFile(Image, "${IMAGE_PATH}");
+        String previewImageName = saveFile(previewImage, "${IMAGE_PATH}");
+
+        if (assetFileName == null || previewImageName == null) {
+            return ResponseEntity.badRequest().body("Failed to save the file.");
+        }
 
         Lot lot = lotDTO.mapToLot(lotDTO);
         lot.setAsset_file(assetFileName);
@@ -34,9 +42,13 @@ public class LotService {
 
         return ResponseEntity.ok("Lot submitted successfully.");
     }
-    private String saveFile(MultipartFile file, String storagePath) {
 
-        String fileName = UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
+    private String saveFile(MultipartFile file, String storagePath) throws IOException {
+        String fileName = UUID.randomUUID().toString();
+        String filePath = storagePath + fileName;
+
+            Files.copy(file.getInputStream(), Paths.get(filePath), StandardCopyOption.REPLACE_EXISTING);
+
 
         return fileName;
     }
